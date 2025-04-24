fe39389477791dbe08553830cdf46de59ff94486
Kevin <simon-kevin@outlook.de>
2025-04-25 00:12:55 +0200

    refactoring

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 
 function Main() {
-  const [ingredients, setIngredients] = React.useState([
-    "Chicken",
-    "Oregano",
-    "Tomatoes",
-  ]);
+  const [ingredients, setIngredients] = React.useState([]);
   const ingredientListItems = ingredients.map((ingredient) => (
     <li>{ingredient}</li>
   ));
 
-  function handleSubmit(event) {
+  function addIngredient(formData) {
     const newIngredient = formData.get("ingredient");
-    console.log(ingredients);
     setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  }
+
+  function hasIngredients() {
+    if(ingredients.length > 0 ) {
+        return true
+    } else {
+        return false
+    }
   }
 
   return (
     <main>
-      <form action={handleSubmit} className="addIngredientForm">
+      <form action={addIngredient} className="addIngredientForm">
         <input
           type="text"
           placeholder="e.g. oregano"
@@ -27,7 +30,17 @@ function Main() {
         />
         <button>Add ingredient</button>
       </form>
-      <ul>{ingredientListItems}</ul>
+      {hasIngredients() && <section>
+        <h2>Ingredients on hand:</h2>
+        <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+        <div className="get-recipe-container">
+            <div>
+              <h3>Ready for a recipe?</h3>
+              <p>Generate a recipe from your list of ingredients.</p>
+            </div>
+            <button>Get a recipe</button>
+        </div>
+      </section>}
     </main>
   );
 }
